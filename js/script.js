58de9fdb44c8459aaaa1451ffb3c8b3007a7d0af
Ohjay25 <joywilson2000@gmail.com>
2023-11-28 21:13:07 +0100

    added images and interactivity

---
+++ js/script.js
@@ -0,0 +1,72 @@
+const change = document.getElementById("dogs");
+
+change.addEventListener("click",(event) => {
+    event.target.style.background = "darksalmon";
+    event.target.style.color = "black";
+});
+
+const change2 = document.getElementById("caucasian");
+change2.addEventListener("click",(event) => {
+    event.target.style.background = "white";
+    event.target.style.color = "black";
+});
+
+const change3 = document.getElementById("corgi");
+change3.addEventListener("click",(event) => {
+    event.target.style.background = "orange";
+    event.target.style.color = "black";
+});
+
+const change4 = document.getElementById("german");
+change4.addEventListener("click",(event) => {
+    event.target.style.background = "tomato";
+    event.target.style.color = "black";
+});
+
+const change5 = document.getElementById("cats");
+change5.addEventListener("click",(event) => {
+    event.target.style.background = "blue";
+    event.target.style.color = "black";
+});
+
+const change6 = document.getElementById("fish");
+change6.addEventListener("click",(event) => {
+    event.target.style.background = "aqua";
+    event.target.style.color = "black";
+});
+
+const change7 = document.getElementById("parrot");
+change7.addEventListener("click",(event) => {
+    event.target.style.background = "crimson";
+    event.target.style.color = "black";
+});
+
+const change8 = document.getElementById("hamster");
+change8.addEventListener("click",(event) => {
+    event.target.style.background = "yellow";
+    event.target.style.color = "black";
+});
+
+const change9 = document.getElementById("iguana");
+change9.addEventListener("click",(event) => {
+    event.target.style.background = "brown";
+    event.target.style.color = "black";
+});
+
+const change10 = document.getElementById("rabbit");
+change10.addEventListener("click",(event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "black";
+});
+
+const change11 = document.getElementById("horse");
+change11.addEventListener("click",(event) => {
+    event.target.style.background = "red";
+    event.target.style.color = "black";
+});
+
+const change12 = document.getElementById("pitbull");
+change12.addEventListener("click",(event) => {
+    event.target.style.background = "darkgray";
+    event.target.style.color = "black";
+});
